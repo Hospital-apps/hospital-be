7aa7414dd7405f674f3a020a65b6ea443b5339e8
christyuda <chrisyuda@ulbi.ac.id>
2024-04-28 20:45:46 +0700

    Add createMedicalCheck function to appointmentController.js

--- src/controllers/appointmentController.js
+++ src/controllers/appointmentController.js
@@ -41,3 +41,47 @@ exports.createAppointment = async (req, res) => {
       });
   }
 };
+
+exports.createMedicalCheck = async (req, res) => {
+  const { time, date, status, package } = req.body;
+  const patientId =  null;
+  const doctorId =  null;
+  const specialtyId =  null;
+  const type = "Offline";
+  try {
+         const newAppointment = new Appointment({
+          patientId ,
+          doctorId,
+          specialtyId,
+          time,
+          date,
+          status,
+          type,
+          package
+      });
+
+      await newAppointment.save();
+      const newHistory = new History({
+        patientId,
+        doctorId,
+        specialtyId,
+        time,
+        date,
+        status,
+        type,
+        package
+    });
+
+    await newHistory.save();  
+
+      res.status(201).json({
+          message: 'Medical Check Up Package created successfully',
+          data: newMedicalCheck
+      });
+  } catch (error) {
+      res.status(500).json({
+          message: 'Failed to create medical check',
+          error: error.message
+      });
+  }
+}
